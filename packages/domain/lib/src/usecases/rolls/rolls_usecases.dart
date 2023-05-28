9a129db5f32fa0d3ebbfefc7d2be4bfe1504261e
devolonter <devolonter@gmail.com>
2023-05-28 17:32:25 +0200

    Small export refactoring

--- packages/domain/lib/src/usecases/rolls/rolls_usecases.dart
+++
@@ -1,3 +0,0 @@
-export 'roll_dice_use_case.dart';
-export 'modify_roll_use_case.dart';
-export 'clear_rolls_use_case.dart';
--- packages/domain/lib/src/usecases/usecases.dart
+++ packages/domain/lib/src/usecases/usecases.dart
@@ -1,1 +1,5 @@
-export 'rolls/rolls_usecases.dart';
+export 'rolls/roll_dice_use_case.dart';
+export 'rolls/modify_roll_use_case.dart';
+export 'rolls/clear_rolls_use_case.dart';
+
+export 'dice/get_all_dice_use_case.dart';
