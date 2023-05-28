bd1d9f40dee32ab792d8098b5992d544fe9a7fdd
devolonter <devolonter@gmail.com>
2023-05-28 19:31:12 +0200

    Added export data classes

---
+++ packages/data/lib/src/repositories/repositories.dart
@@ -0,0 +1,2 @@
+export 'dice_repository_impl.dart';
+export 'rolls_repository_impl.dart';
---
+++ packages/data/lib/src/sources/sources.dart
@@ -0,0 +1,5 @@
+export 'dice_data_source.dart';
+export 'rolls_data_source.dart';
+
+export 'static/static_dice_data_source.dart';
+export 'isar/isar_rolls_data_source.dart';
