d1335934c1a3b6fa0d1d7af303ae460e832f0241
devolonter <devolonter@gmail.com>
2023-05-28 18:26:11 +0200

    Implemented Isar rolls data source

---
+++ packages/data/lib/src/sources/isar/isar_rolls_data_source.dart
@@ -0,0 +1,31 @@
+import 'package:domain/src/entities/roll.dart';
+import 'package:isar/isar.dart';
+
+import '../../models/isar/isar_roll_model.dart';
+import '../rolls_data_source.dart';
+
+class IsarRollsDataSource implements RollsDataSource {
+  Isar _isar;
+
+  IsarRollsDataSource(this._isar);
+
+  @override
+  Future<List<Roll>> fetchAll() async {
+    final List<IsarRollModel> isarRolls =
+        await _isar.collection<IsarRollModel>().where().findAll();
+    return isarRolls.map((e) => e.toEntity()).toList();
+  }
+
+  @override
+  Future<void> save(Roll roll) {
+    return _isar.writeTxn(() async {
+      await _isar
+          .collection<IsarRollModel>()
+          .put(IsarRollModel.fromEntity(roll));
+    });
+  }
+
+  @override
+  Future<void> clear() =>
+      _isar.writeTxn(() => _isar.collection<IsarRollModel>().clear());
+}
