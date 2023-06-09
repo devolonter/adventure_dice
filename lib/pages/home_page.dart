79fc18f8dac84472c30dc35e0ac053ac01d67b78
devolonter <devolonter@gmail.com>
2023-06-09 17:06:13 +0200

    Updated home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,35 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/dice.dart';
+import '../widgets/dice/dice_grid.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final config = ref.watch(appDiceProvider);
+
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Adventure Dice'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-          ),
-          itemCount: config.value?.length ?? 0,
-          itemBuilder: (context, index) {
-            final dice = config.value![index];
-            return Card(
-              child: Center(
-                child: Text(
-                  dice.sides.toString()
-                ),
-              ),
-            );
-          }
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(child: SizedBox.shrink()),
+            DiceGrid(),
+          ],
         ),
       ),
     );
