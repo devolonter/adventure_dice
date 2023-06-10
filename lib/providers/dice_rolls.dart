eab9b985842b8faa9e4531cab76951c0b46c1423
devolonter <devolonter@gmail.com>
2023-06-10 11:42:49 +0200

    Added dice rolls providers

---
+++ lib/providers/dice_rolls.dart
@@ -0,0 +1,42 @@
+
+import 'package:data/data.dart';
+import 'package:domain/domain.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'isar.dart';
+
+part 'dice_rolls.g.dart';
+
+@riverpod
+class DiceRolls extends _$DiceRolls {
+  late final RollsRepository _rollsRepository;
+  late final GetAllRolls _getAllRolls;
+  late final RollDice _rollDice;
+
+  @override
+  Future<List<Roll>> build() async {
+    final isar = await ref.watch(isarProvider.future);
+
+    _rollsRepository = RollsRepositoryImpl(
+      IsarRollsDataSource(isar)
+    );
+
+    _getAllRolls = GetAllRolls(_rollsRepository);
+    _rollDice = RollDice(_rollsRepository);
+
+    return _getAllRolls();
+  }
+
+  Future<void> rollDice(Dice dice) async {
+    state = await AsyncValue.guard(() async {
+      await _rollDice(dice);
+      return _getAllRolls();
+    });
+  }
+}
+
+@riverpod
+AsyncValue<Roll?> getLatestRoll(GetLatestRollRef ref) {
+  final rolls = ref.watch(diceRollsProvider);
+  return rolls.whenData((rolls) => rolls.lastOrNull);
+}
---
+++ lib/providers/dice_rolls.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dice_rolls.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getLatestRollHash() => r'013535f405f3eff50fd9dc2ec6cca50938dc0c59';
+
+/// See also [getLatestRoll].
+@ProviderFor(getLatestRoll)
+final getLatestRollProvider = AutoDisposeProvider<AsyncValue<Roll?>>.internal(
+  getLatestRoll,
+  name: r'getLatestRollProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getLatestRollHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetLatestRollRef = AutoDisposeProviderRef<AsyncValue<Roll?>>;
+String _$diceRollsHash() => r'28fc16316bd19af59de1d60231320b13d198b7cc';
+
+/// See also [DiceRolls].
+@ProviderFor(DiceRolls)
+final diceRollsProvider =
+    AutoDisposeAsyncNotifierProvider<DiceRolls, List<Roll>>.internal(
+  DiceRolls.new,
+  name: r'diceRollsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$diceRollsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DiceRolls = AutoDisposeAsyncNotifier<List<Roll>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
