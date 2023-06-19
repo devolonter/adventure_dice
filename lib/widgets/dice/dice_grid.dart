827dba4d389bba3158ad52063fcdd2d11fc1ad05
devolonter <devolonter@gmail.com>
2023-06-19 22:50:48 +0200

    Changed the dice grid to use the provider in the right way

--- lib/widgets/dice/dice_grid.dart
+++ lib/widgets/dice/dice_grid.dart
@@ -14,30 +14,35 @@ class DiceGrid extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final AsyncValue<List<Die>> config = ref.watch(diceListProvider);
+    final AsyncValue<List<Die>> dice = ref.watch(diceListProvider);
 
-    final int countDice = config.value?.length ?? 0;
-    final double dieSize = MediaQuery.of(context).size.width / crossAxisCount;
-    final int countDicePerRow = (countDice / crossAxisCount).ceil();
+    return dice.maybeWhen(
+      data: (config) {
+        final int countDice = config.length;
+        final double dieSize = MediaQuery.of(context).size.width / crossAxisCount;
+        final int countDicePerRow = (countDice / crossAxisCount).ceil();
 
-    return Container(
-      height: dieSize * countDicePerRow +
-          (padding / 2) * (countDicePerRow - 1) +
-          padding * 2,
-      color: Theme.of(context).colorScheme.primary.withOpacity(0.33),
-      padding: EdgeInsets.all(padding.toDouble()),
-      child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: crossAxisCount,
-            crossAxisSpacing: padding.toDouble(),
-            mainAxisSpacing: padding.toDouble(),
-          ),
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: countDice,
-          itemBuilder: (context, index) {
-            final dice = config.value![index];
-            return DieSingle(die: dice, size: Size.square(dieSize));
-          }),
+        return Container(
+          height: dieSize * countDicePerRow +
+              (padding / 2) * (countDicePerRow - 1) +
+              padding * 2,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.33),
+          padding: EdgeInsets.all(padding.toDouble()),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                crossAxisSpacing: padding.toDouble(),
+                mainAxisSpacing: padding.toDouble(),
+              ),
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: countDice,
+              itemBuilder: (context, index) {
+                final die = config[index];
+                return DieSingle(die: die, size: Size.square(dieSize));
+              }),
+        );
+      },
+      orElse: () => const SizedBox.shrink(),
     );
   }
 }
